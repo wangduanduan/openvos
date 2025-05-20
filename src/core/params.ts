ea873590cbb64912755708685680070a70c4cf7a
wangduanduan <wangduanduan@bjsxai.com>
2025-05-20 19:10:34 +0800

    update

--- src/core/params.ts
+++ src/core/params.ts
@@ -1,27 +1,26 @@
 import { set_logger_level } from './logger'
 
-export interface params {
-    logLevel?: string
-    minMsgLen?: number
-    maxMsgLen?: number
+export interface Params {
+    logLevel: string
+    minMsgLen: number
+    maxMsgLen: number
 }
 
-export const coreParams: params = {
+export const coreParams: Params = {
     logLevel: 'debug',
     minMsgLen: 16,
     maxMsgLen: 5060,
 }
 
-export function setCoreParams(cp: params) {
-    Object.assign(coreParams, cp)
+export function setCoreParams<T extends keyof Params>(k: T, v: Params[T]): void {
+    // Object.assign(coreParams, cp)
 
-    for (const name in coreParams) {
-        switch (name) {
-            case 'logLevel':
-                set_logger_level(coreParams[name]!)
-                break
-            default:
-                continue
-        }
+    coreParams[k] = v
+
+
+    switch (k) {
+        case 'logLevel':
+            set_logger_level(coreParams.logLevel)
+            break
     }
 }
